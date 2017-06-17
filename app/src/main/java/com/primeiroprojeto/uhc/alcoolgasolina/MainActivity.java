da59a3bd2151a115951b5ffedc17f7441da6dba4
uhconst <uryelhenrique.c@gmail.com>
2017-06-17 15:31:38 -0300

    Prototipando o ListView e as preferencias compartilhadas pra salvar o historico.

--- app/src/main/java/com/primeiroprojeto/uhc/alcoolgasolina/MainActivity.java
+++ app/src/main/java/com/primeiroprojeto/uhc/alcoolgasolina/MainActivity.java
@@ -1,16 +1,29 @@
 package com.primeiroprojeto.uhc.alcoolgasolina;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String NOME_ARQUIVO = "historico.txt";
 
     private Button btnCalcular;
 
@@ -22,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView listaHistorico;
 
+    private ArrayList<String>  historicoResultado = new ArrayList<>();
+
+    private String [] Test = {"Test1","Test2"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +52,13 @@ public class MainActivity extends AppCompatActivity {
         txtResultado = (TextView) findViewById(R.id.txtResultado);
 
         listaHistorico = (ListView) findViewById(R.id.listViewHistorico);
-        
+
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //Convertendo valores para float para fazer os calculos
                 final float valorAlcool = Float.valueOf(txtValueAlcool.getText().toString());
-
                 final float valorGasolina = Float.valueOf(txtValueGasolina.getText().toString());
 
                 if((valorAlcool/valorGasolina)<0.7) {
@@ -52,7 +68,94 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     txtResultado.setText("Compensa Gasolina!");
                 }
+
+                //Salvando o resultado no arquivo
+                gravarNoArquivo(String.valueOf(txtResultado.getText()));
+
+                Toast.makeText(MainActivity.this, "Calculado e salvo no histórico.", Toast.LENGTH_SHORT).show();
+
+                preencherListView();
             }
         });
+
+        preencherListView();
+
+
     }
+
+    private void preencherListView() {
+
+        Log.d(">>preencherListView", "Dentro do metodo");
+        //Recuperando o que foi gravado
+        if(lerHistorico()!=null){
+            Log.d(">>preencherListView", "Nao é nulo");
+            ArrayAdapter<String> adaptador = new ArrayAdapter<>(
+                    getApplicationContext(),
+                    android.R.layout.simple_list_item_1,
+                    android.R.id.text1,
+                    historicoResultado
+            );
+
+            listaHistorico.setAdapter(adaptador);
+
+            Log.d(">>preencherListView", String.valueOf(historicoResultado.size()));
+        }
+    }
+
+    private ArrayList<String> lerHistorico(){
+
+        //String[] historicoResultado;
+
+        try {
+            InputStream historico = openFileInput(NOME_ARQUIVO);
+
+            //Checando se o arquivo existe
+            if(historico!=null){
+
+                Log.d(">>lerHistorico", "Arquivo existe");
+                //Lendo o arquivo
+                InputStreamReader inputStreamReader = new InputStreamReader(historico);
+
+                //Gerando buffer do arquivo lido
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                //Recuperando textos do arquivo
+                bufferedReader.readLine();
+
+                String linhaHistorico = "";
+
+                //Percorrendo o arquivo enquanto tiver texto no buffer
+                while( (linhaHistorico = bufferedReader.readLine()) != null ){
+                    //historicoResultado +=  linhaHistorico;
+                    Log.d(">>>lerHistorico", "Dentro do while");
+                    historicoResultado.add(linhaHistorico);
+                }
+            }
+
+            historico.close();
+
+
+        }
+        catch (IOException e){
+            Log.v("MainActivity", e.toString());
+        }
+
+        return historicoResultado;
+    }
+
+    private void gravarNoArquivo(String resultado) {
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(NOME_ARQUIVO, Context.MODE_PRIVATE));
+            outputStreamWriter.write(resultado);
+            outputStreamWriter.close();
+
+            Log.d(">>gravarNoArquivo", "Gravando no arquivo");
+        }
+        catch (IOException e){
+            Log.v("MainActivity", e.toString());
+        }
+
+    }
+
 }
